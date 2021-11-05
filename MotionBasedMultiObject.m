30e958a631e6d28faf8535f17f15d47d313a8f6c
Saba <73706196+ESKA17@users.noreply.github.com>
2021-11-05 21:33:30 +0600

    Angle revision

--- MotionBasedMultiObject.m
+++ MotionBasedMultiObject.m
@@ -1,10 +1,9 @@
-close all; imaqreset
-MotionBasedMultiObject();
 function MotionBasedMultiObject()
 % Create System objects used for reading video, detecting moving objects,
 % and displaying the results.
+close all; imaqreset
 vid = videoinput('winvideo');
-triggerconfig(vid,'manual'); set(vid,'TriggerRepeat',inf);
+triggerconfig(vid, 'manual'); set(vid, 'TriggerRepeat', inf);
 vid.FrameGrabInterval = 1;
 src = getselectedsource(vid); frameRates = set(src, 'FrameRate');
 fps = frameRates{1}; src.FrameRate = fps;
@@ -16,11 +15,11 @@ nextId = 1; % ID of the next track
 numFrames = 10000;
 % Detect moving objects, and track them across video frames.
 start(vid); frame_count = 1;
-figure('Name','Video capture','NumberTitle','off')
-while frame_count < numFrames+1
-    trigger(vid);
-    frame = getsnapshot(vid);
-    [bboxes, centroids, pts, linepts, myang, refbbox] = improc();
+warning('off')
+figure('Name','Video capture', 'NumberTitle','off')
+while frame_count < numFrames + 1
+    trigger(vid); frame = getsnapshot(vid);
+    [bboxes, centroids, pts, linepts, myang, crns, refbbox] = improc();
     predictNewLocationsOfTracks();
     [assignments, unassignedTracks, unassignedDetections] = ...
         detectionToTrackAssignment();
@@ -31,24 +30,76 @@ while frame_count < numFrames+1
     frame = displayTrackingResults(frame);
     imshow(frame)
     frame_count = frame_count + 1; 
+    flushdata(vid);
 end
-stop(vid); flushdata(vid); delete(vid);
+stop(vid);  delete(vid);
 
 %% Image processing
-function [bboxes, centroids, pts, linepts, myang, refbbox] = improc()
-% Finding sticker
-Isub = imsubtract(frame(:,:,1), rgb2gray(frame));
+function [bboxes, centroids, pts, linepts, myang, crns, refbbox, Isub] = improc()
+    
+% Finding a sticker
+Isub = imsubtract(frame(:,:,1), rgb2gray(frame)); % red one
 Isub = imgaussfilt(Isub, 2);
 Isub = imbinarize(Isub);
-% Isub = bwareaopen(Isub, 300);
-regprops = regionprops(Isub, 'Orientation', 'BoundingBox');
+Isub = bwareaopen(Isub, 300);
+regprops = regionprops(Isub, 'Orientation', 'BoundingBox', 'Area');
+areaFromStruct = [regprops.Area];
+g = areaFromStruct < 750;
+regprops = regprops(g);
+linepts = [0 0 0 0]; myang = 0;
 if ~isempty(regprops)
-refbbox = regprops.BoundingBox;
-linepts = [refbbox(1) refbbox(2)+refbbox(4) ... 
-    refbbox(1)+refbbox(3) refbbox(2)];
-myang = regprops.Orientation;
-else
-    linepts = [0 0 0 0]; myang = 0;
+%     linepts = [refbbox(1) refbbox(2)+refbbox(4) ...
+%         refbbox(1)+refbbox(3) refbbox(2)];
+%     myang = regprops.Orientation;
+%     Isubcrp = imcrop(Isub, refbbox);
+    refbbox = extractfield(regprops, 'BoundingBox');
+    refbbox = reshape(refbbox, [4, size(regprops, 1)])';
+    % Cut out
+    Isubcrp = cell([1, size(regprops, 1)]);
+    crns = cell([1, size(regprops, 1)]);
+    for i = 1:size(regprops, 1)
+        Isubcrp{i} = imcrop(Isub, refbbox(i, :));
+        crns{i} = pgonCorners(Isubcrp{i}, 5);
+    end
+    %Examine each one
+    for ii = 1:size(regprops, 1)    
+        if size(crns{ii}, 1) == 5
+            angle = zeros(5, 1);
+            ctmp = cat(1, crns{ii}, crns{ii}(1:2, :));
+            for c = 1:5
+                % Normalized vectors
+                n1 = (ctmp(c+2,:)-ctmp(c,:)) / ...
+                    norm(ctmp(c+2,:)-ctmp(c,:));
+                n2 = (ctmp(c+1,:) - ctmp(c,:)) / ...
+                    norm(ctmp(c+1,:) - ctmp(c,:));
+                angle(c) = rad2deg(atan2(norm(det([n2; n1])),dot(n1, n2)));
+%  angle(c) = rad2deg(atan2(abs((ctmp(c+1,1)-ctmp(c,1))*(ctmp(c+2,2)-ctmp(c,2))-(ctmp(c+2,1)-ctmp(c,1))*(ctmp(c+1,2)-ctmp(c,2))), ...
+%                 (ctmp(c+1,1)-ctmp(c,1))*(ctmp(c+2,1)-ctmp(c,1))+(ctmp(c+1,2)-ctmp(c,2))*(ctmp(c+2,2)-ctmp(c,2))));
+            end
+            angle
+            str = find(angle < 95 & angle > 85);
+            shr = find(angle < 50);
+            if length(str) == 2 && length(shr) == 1
+                linepts = ...
+                    [abs(ctmp(str(1)+1,1)-ctmp(str(2)+1,1))/2+...
+                    min(ctmp(str(1)+1,1)-ctmp(str(2)+1,1)), ...
+                    abs(ctmp(str(1)+1,2)-ctmp(str(2)+1,2))/2 + ...
+                    min(ctmp(str(1)+1,2)-ctmp(str(2)+1,2)), ...
+                    ctmp{ii}(shr + 1, 1), ctmp(shr + 1, 2)];
+                slope = (linepts(3) - linepts(1))./ ...
+                    (linepts(2) - linepts(4));
+                myang = atand(slope)
+                crns{ii}(:, 1) = ctmp(:, 1)+ refbbox(ii, 1); 
+                crns{ii}(:, 2) = ctmp(:, 2)+refbbox(ii, 2)-refbbox(ii, 4); 
+                refbbox = refbbox(ii, :);
+            end
+        end
+    end
+end
+chck1 = exist('crns', 'var');
+chck2 = exist('refbbox', 'var');
+if  chck1 ~= 1 || chck2 ~=1
+    crns = []; refbbox = [];
 end
     Img = rgb2gray(frame);
     % A set of filters
@@ -66,7 +117,7 @@ end
     filtreg = roipoly(Img, [linepts(1)-ov linepts(1)-ov ...
         linepts(3)+ov linepts(3)+ov],...
         [linepts(2)+ov linepts(4)-ov linepts(4)-ov linepts(2)+ov]);
-    Img = roifilt2(h,Img,filtreg);
+    Img = roifilt2(h, Img, filtreg);
     out = edge(Img, 'Canny', [0.001 0.2]);
 %     out = bwareaopen(out, 30);
 %     out = imclose(out, 50);
@@ -75,7 +126,7 @@ end
     [H,T,R] = hough(out, 'RhoResolution', 1);
     P  = houghpeaks(H, 1);
     UnqLines = houghlines(out, T, R, P, 'MinLength', 100);
-    out = uint8(repmat(out, 1, 1, 3)) .* 255;
+%     out = uint8(repmat(out, 1, 1, 3)) .* 255;
     
     % Calculating bounding boxes, centroids and straight line coefficients
     numUnqLines = length(UnqLines); mnan = [];
@@ -113,7 +164,7 @@ end
         end
     end
     
-    % Filtering close to each other centroids
+    % Filtering out close to each other centroids
     tmpk = []; oldl = 2; newl = 1;
     while oldl > newl && size(centroids, 1) > 1
         oldl = size(centroids, 1);
@@ -130,12 +181,10 @@ end
     end  
     
     % Creating visual markers for mask video
-    out = insertShape(out, 'Line', [point1 point2], ...
-                'LineWidth', 3, 'Color', 'green', 'SmoothEdges', false);
-    out = insertMarker(out, centroids, 'o', 'Size', 10, ...
-                'Color', 'red');
-    out = insertShape(out, 'Line', linepts, ...
-                'LineWidth', 3, 'Color', 'green', 'SmoothEdges', false);
+%     out = insertShape(out, 'Line', [point1 point2], ...
+%                 'LineWidth', 3, 'Color', 'green', 'SmoothEdges', false);
+%     out = insertMarker(out, centroids, 'o', 'Size', 10, ...
+%                 'Color', 'red');
 end
 %% Creating an empty array of tracks
 function tracks = initializeTracks()
@@ -301,7 +350,11 @@ function frame = displayTrackingResults(frame)
                 position(i, :) = [1, 200+60*(i-1)];
             end
             if ~isequal(linepts, [0, 0, 0, 0])
-            angle = angle + myang-90;
+            angle = angle + myang - 90;
+            frame = insertShape(frame, 'Line', linepts, ...
+                'LineWidth', 3, 'Color', 'green', 'SmoothEdges', false);
+            frame = insertShape(frame, 'Rectangle', refbbox, ...
+                'LineWidth', 3, 'Color', 'green', 'SmoothEdges', false);
             end
             angle = cellstr(int2str(angle'));
             
@@ -324,12 +377,13 @@ function frame = displayTrackingResults(frame)
                 [bboxes(:, 1)+bboxes(:, 3)/2, ...
                 bboxes(:, 2)-bboxes(:, 4)/2, ...
                 5*ones(size(bboxes, 1), 1)], labels);
+            if size(crns, 1)>3 && ~isempty(crns)
+                v = crns(:, 2); crns(:, 2) = crns(:, 1); crns(:, 1) = v;
+                frame = insertShape(frame, 'Polygon', ...
+                    reshape(crns.',1,[]));
+            end
             frame = insertText(frame, position, anglelabels, ...
                 'FontSize', 30, 'BoxColor', 'green');
-            frame = insertShape(frame, 'Line', linepts, ...
-                'LineWidth', 3, 'Color', 'green', 'SmoothEdges', false);
-            frame = insertShape(frame, 'Rectangle', refbbox, ...
-                'LineWidth', 3, 'Color', 'green', 'SmoothEdges', false);
         end
     end
     
